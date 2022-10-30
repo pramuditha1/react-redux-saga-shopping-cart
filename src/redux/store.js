41dae3c617bfe51de6dda5214c6a0a18a857a26a
Pramuditha <pramuditha.niroshan@circles.asia>
2022-10-30 06:53:19 +0530

    Add reduxjs/toolkit to store and replace createStore with configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
+// createStore should replace with configureStore provided by redux/toolkit
+import { configureStore } from '@reduxjs/toolkit';
+
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer)
+const store = configureStore({reducer: rootReducer})
 
 export default store;
