7a77e78c4d7232069dca33f0933b8835d6882819
Pramuditha <pramuditha.niroshan@circles.asia>
2022-10-31 07:59:54 +0530

    Add redux saga to fetching products

--- src/redux/productActions.js
+++ src/redux/productActions.js
@@ -1,10 +1,7 @@
 import { PRODUCT_LIST } from './constants'
 
 export const getProductList = () => {
-    let data = "hello this is product list"
-    console.log('PRODUCT_LIST action called', data);
     return {
         type: PRODUCT_LIST,
-        data
     }
 }
--- src/redux/sagas/productSaga.js
+++ src/redux/sagas/productSaga.js
@@ -1,5 +1,13 @@
-function* productSaga() {
+import { takeEvery, put } from 'redux-saga/effects'
+import { PRODUCT_LIST, SET_PRODUCT_LIST } from '../constants'
 
+function* getProductList() {
+    let data = yield fetch("http://localhost:3500/products")
+    data = yield data.json()
+}
+
+function* productSaga() {
+    yield takeEvery(PRODUCT_LIST, getProductList)
 }
 
 export default productSaga
