3df77ec9477473180f898a81e3409fad30177c26
Pramuditha <pramuditha.niroshan@circles.asia>
2022-10-31 12:19:27 +0530

    SET_PRODUCTS to store

--- src/redux/productReducer.js
+++ src/redux/productReducer.js
@@ -1,14 +1,14 @@
-import { PRODUCT_LIST } from './constants'
+import { PRODUCT_LIST, SET_PRODUCT_LIST } from './constants'
 
 export const productList = (data = [], action) => {
     console.warn('reducer is called action : ' , action);
     // implement reducer logic using a switch statement
     switch(action.type) {
-        case PRODUCT_LIST:
-            console.log('reducer called PRODUCT_LIST')
-            return [action.data]
+        case SET_PRODUCT_LIST:
+            console.log('reducer called SET_PRODUCT_LIST')
+            return [...action.data]
         default:
             console.log('reducer called no action')
-            return []
+        return []
     }
 }
