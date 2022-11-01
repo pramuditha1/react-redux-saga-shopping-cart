7c1d30224642e09520449a5d0c6b527adfae6de9
Pramuditha <pramuditha.niroshan@circles.asia>
2022-11-01 22:41:38 +0530

    Show cart items in JSON format in cart element

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function Cart() {
+    const cartData = useSelector((state) => state.cartData)
   return (
-    <div>Cart component</div>
+    <div>
+        {
+            cartData.map(
+                (cartItem) => (
+                    <p>{JSON.stringify(cartItem)}</p>
+                )
+            )
+        }
+    </div>
   )
 }
 
